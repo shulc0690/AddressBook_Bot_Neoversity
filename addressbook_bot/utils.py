2036c13a856a731546a8cf82d50209c77b03b766
andrii <reading_dragon_0t@icloud.com>
2024-08-19 11:56:03 +0200

    updated print_the_contact_in_table and few changes from black

--- addressbook_bot/utils.py
+++ addressbook_bot/utils.py
@@ -179,56 +179,11 @@ def search_contact(args, book: AddressBook):
     return print_contacts_table(records)
 
 def print_the_contact_in_table(record):
-    table = Table(
-        title="TARGET",
-        show_lines=True,
-        title_style="green",
-        border_style="green",
-    )
-    table.add_column("Name", style="cyan", no_wrap=True, header_style="green")
-    table.add_column("Last Name", style="cyan", header_style="green")
-    table.add_column("Phone", style="green", header_style="green")
-    table.add_column("Email", style="cyan", header_style="green")
-    table.add_column("Address", style="blue", header_style="green")
-    table.add_column("Birthday", style="yellow", header_style="green")
-    table.add_column("Notes", style="bright_black", header_style="green")
+    tmp_book = AddressBook()
+    tmp_book.add_record(record)
 
-    last_name_str = record.last_name if record.last_name else "No last name"
-    phones_str = (
-        "; ".join(p.value for p in record.phones) if record.phones else "No phone"
-    )
-    email_str = (
-        record.email.value
-        if hasattr(record, "email") and record.email
-        else "No email"
-    )
-    address_str = (
-        record.address.value
-        if hasattr(record, "address") and record.address
-        else "No address"
-    )
-    if isinstance(record.birthday, Birthday):
-        birthday_str = record.birthday.value.strftime("%d.%m.%Y")
-    elif isinstance(record.birthday, str):
-        birthday_str = record.birthday
-    else:
-        birthday_str = "Alive yet"
-    notes_str = "\n".join(
-        f"{i+1}. {note.title}: {note.content}"
-        for i, note in enumerate(record.notes)
-    )
-    if not notes_str:
-        notes_str = "No notes"
-    table.add_row(
-        record.name.value,
-        last_name_str,
-        phones_str,
-        email_str,
-        address_str,
-        birthday_str,
-        notes_str,
-    )
-    console.print(table)
+    print_contacts_table(tmp_book)
+    del tmp_book
 
 def print_contacts_table(book):
     table = Table(
@@ -281,6 +236,7 @@ def print_contacts_table(book):
             notes_str,
         )
     console.print(table)
+    return info_msg4return("All targets in table. What's next?")
 
 
 def show_address_book(book: AddressBook):
@@ -784,20 +740,22 @@ def delete_phone(args, book):
             main_msg(f"{i+1}. Back to main menu")
 
             phone_number_choice = input(
-                    main_msg4return(
-                        "Enter the number of the phone you want to delete: "
-                    )
-                ).strip()
+                main_msg4return("Enter the number of the phone you want to delete: ")
+            ).strip()
             if phone_number_choice.isdigit():
                 phone_number_choice = int(phone_number_choice)
                 if 1 <= phone_number_choice <= len(record.phones):
                     phone2remove = record.phones[phone_number_choice - 1].value
                     try:
                         if not record.find_phone(phone2remove):
-                            return info_msg4return(f"{name} does not have this phone number.")
+                            return info_msg4return(
+                                f"{name} does not have this phone number."
+                            )
 
                         record.remove_phone(phone2remove)
-                        return info_msg4return(f"Phone number {phone2remove} for {name} has been deleted.")
+                        return info_msg4return(
+                            f"Phone number {phone2remove} for {name} has been deleted."
+                        )
                     except ValueError as e:
                         error_msg(f"Error: {e}.")
                 elif phone_number_choice == len(record.phones) + 1:
@@ -813,9 +771,12 @@ def delete_phone(args, book):
                     return info_msg4return(f"{name} does not have this phone number.")
 
                 record.remove_phone(phone2remove)
-                return info_msg4return(f"Phone number {phone2remove} for {name} has been deleted.")
+                return info_msg4return(
+                    f"Phone number {phone2remove} for {name} has been deleted."
+                )
             except ValueError as e:
                 error_msg(f"Error: {e}.")
+
 
 @input_error
 def delete_contact(args, book: AddressBook):
